b9c910e403926283d6f5bc9b35719a96d257d398
santyrgh <santiagoguerreroh034@gmail.com>
2023-08-25 11:23:37 -0500

    jola

--- app.py
+++ app.py
@@ -1,16 +1,72 @@
-from flask import Flask, render_template
-from rutas.routes import configure_routes
-from conexion.db_connection import create_db_connection
+from flask import Flask, render_template , request
+from flaskext.mysql import MySQL
+from datetime import datetime
 
 app = Flask(__name__)
 
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'desdecero'
+mysql.init_app(app)
 
-mysql = create_db_connection(app)
-configure_routes(app)
 
 
 
 
+@app.route('/add', methods=['POST'])
+def add():
+    nombre = request.form['nombre']
+    apellido = request.form['apellido']
+
+    # Obtener una conexión
+    con = mysql.connect()
+    cursor = con.cursor()
+
+    # Realizar operaciones con la base de datos
+    cursor.execute('INSERT INTO registro (nombre, apellido) VALUES (%s, %s)',
+                   (nombre, apellido))
+    con.commit()
+
+    # Cerrar cursor y conexión
+    cursor.close()
+    con.close()
+
+    return "ok"
+
+
+
+
+
+
+
+
+
+
+@app.route('/registrar', methods=['POST'])
+def registro():
+    nombre = request.form['nombre']
+    email = request.form['email']
+    clave = request.form['claves']
+
+    # Obtener una conexión
+    con = mysql.connect()
+    cursor = con.cursor()
+
+    # Realizar operaciones con la base de datos
+    cursor.execute('INSERT INTO registro (nombre, email, clave) VALUES (%s, %s,%s)',
+                   (nombre, email , clave))
+    con.commit()
+
+    # Cerrar cursor y conexión
+    cursor.close()
+    con.close()
+
+    return "ok"
+
+
+    
 
 
 
@@ -18,6 +74,12 @@ configure_routes(app)
 def hola():
     return render_template('index.html')
 
+
+
+
+@app.route('/datos')
+def datos():
+    return render_template('registro.html')
 
 
 if __name__ == '__main__':
--- rutas/home.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template , render_template
-
-
-routes_home = Blueprint("routes_home", __name__)
-
-#ruta del formulario
-@routes_home.route('/foto', methods=['GET'])
-def formularios():
-    return render_template('template.html')
